dfc6f5ea90edfa5044511c2981f52d4d86c9991a
Daniel Hartnell <danielhartnell@gmail.com>
2016-01-23 12:22:24 -0800

    correctly ignore DS_Store

--- public/javascript/interface.js
+++ public/javascript/interface.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
   $("#configure-worksheets").click(function() {
     $("#config.hide-me").slideToggle();
   });
+
+  var n = $("#number option:selected").text();
+  
+  $("#get-worksheets").click(function() {
+    $.post("/mkpdf?enumb=" + 12);
+  });
 });
