aae923b7ad8d02b5f4714a096b0ab97a19a7fe5c
Daniel Hartnell <daniel@Daniels-MacBook-Pro.local>
2016-01-29 16:30:26 -0800

    trying to implement single responsibility

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var genWs = require('../handlers/genws');
+var worksheet = require('../lib/pdf/build-pdf.js');
 
-var fs = require('fs');
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Teacher Toolkit' });
 });
 
-/* GET dynamically generated PDF */
 router.get('/one-step-pdf', function(req, res) {
-  // Response headers
   res.setHeader('Content-disposition', 'attachment; filename=equations.pdf');
   res.setHeader('Content-type', 'application/pdf');
-
-  // User specified parameters
-  var numEquations = req.query.numEquations > 50 ? 50 : req.query.numEquations;   // keeps it under 50
-
-  genWs.genPDF(numEquations, req.query.complexity, req.query.answerKey, function(pdf) {
-    // Pipe document directly into response
+  
+  // Limit the number of equations
+  var numEquations = req.query.numEquations > 50 ? 50 : req.query.numEquations;
+  worksheet.build("one-step", numEquations, req.query.complexity, req.query.answerKey, function(pdf) {
+    console.log('I got into the worksheet.build function');
     pdf.pipe(res);
-
-    // Response is closed making res.end() redundant
     pdf.end();
   });
 });
